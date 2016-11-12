879fed8fb8213f1337c6046aaf628899f4499250
Джалиль Акбашев <jaleel@MacBook-Pro-Dzalil.local>
2016-11-13 00:14:35 +0300

    Bubble sort index fix

--- src/algorithm/sort/bubble/BubbleSort.kt
+++ src/algorithm/sort/bubble/BubbleSort.kt
@@ -3,14 +3,14 @@ package algorithm.sort.bubble
 /**
  * Created by jaleel on 11.11.16.
  */
+
 fun <T> bubbleSort(array: Array<T>): Array<T> where T : Comparable<T> {
     if (array.size <= 1) {
         return array
     }
-
     val arr = array
     for (i in 0..arr.size-1) {
-        for (j in 1..arr.size-i-2) {
+        for (j in 0..arr.size-i-2) {
             if (arr[j] > arr[j+1]) {
                 val tmp: T = arr[j]
                 arr[j] = arr[j+1]
